ed8498fb127da8883858a2931c7742b8fd16265c
Hinka Patel <hinkapatel2994@gmail.com>
2021-05-06 15:33:58 -0700

    mongoDB exercise

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -46,9 +46,48 @@ async function getCourses() {
     .skip( (pageNumber - 1) * pageSize )
     .limit(pageSize)
     .sort({ name: 1}) // 1 means ascending order and -1 means decending order.
-    .count(); // it returns the count of documents that match find criteria.
+    .count() // it returns the count of documents that match find criteria.
     .select({ name: 1, tags: 1}); // we can select the properties that we want to be returned.
   console.log(courses);
 }
 
-getCourses();
+//Approach: Quesry First --> findbyId() - Modify its properties - save
+//use: if we receive an input from client and we want to make sure that the update is valid operation for example we have to check
+// isPublished is true or not and if it true then don't update it so we can simply return if it is true.
+/*async function updateCourse(id) {
+  const course = await Course.findById(id);
+  if (!course) return;
+
+  //course.isPublished = true;
+  //course.author = 'Another Author';
+
+  course.set({
+    isPublished: true,
+    author: 'Another author'
+  });
+  const result = await course.save();
+  console.log(result);
+}*/
+
+//Approach: Update First --> update directly - optionally(get the updated documents)
+async function updateCourse(id) {
+  const result = await Course.update({ _id: id } , {
+    $set: {
+      author: 'Aaria',
+      isPublished: false
+    }
+  });
+  console.log(result);
+}
+
+//updateCourse('609381c6b695ae149463037d');
+
+//to delete multiple documents use deleteMany
+async function removeCourse(id) {
+  const result = await Course.deleteOne({ _id: id });
+  //if we want to get the document that was deleted
+  //const course await Course.findByIdAndRemove(id);
+  console.log(result);
+}
+
+removeCourse('609381c6b695ae149463037d');
