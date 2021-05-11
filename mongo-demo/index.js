48c3d8038ae66c4c68f8d3cfca755e30c0672deb
Hinka Patel <hinkapatel2994@gmail.com>
2021-05-10 17:11:06 -0700

    Add Persistence to Genres API

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to Mongo DB...', err));
+  .catch(err => console.error('Could not connect to MongoDB...', err));
 
 const courseSchema = new mongoose.Schema({
   name: {
--- vidly/index.js
+++ vidly/index.js
@@ -1,7 +1,11 @@
-const Joi = require('joi');
+const mongoose = require('mongoose');
 const express = require('express');
 const genres = require('./routes/genres');
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.log('Could not connect to MongoDB...', err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
