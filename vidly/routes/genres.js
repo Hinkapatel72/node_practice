bad19eca785fabd3b92b242ee2a0c94ad59c0780
Hinka Patel <hinkapatel2994@gmail.com>
2021-05-10 20:58:22 -0700

    Build the Customers API

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -1,18 +1,7 @@
-const Joi = require('joi'); //to validate the genres the client sends us.
+const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-
-const genreSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-});
-
-const Genre = mongoose.model('Genre', genreSchema);
 
 
 router.get('/', async (req, res) => {
@@ -21,7 +10,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const result = validateGenre(req.body);
+  const result = validate(req.body);
   if (result.error) return res.status(400).send(result.error.details[0].message);
 
   let genre = new Genre ({ name: req.body.name });
@@ -31,7 +20,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateGenre(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
@@ -59,13 +48,5 @@ router.get('/:id', async (req, res) => {
 
   res.send(genre);
 });
-
-function validateGenre(genre) {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(genre, schema);
-}
 
 module.exports = router;
